802e959edb089e9f387a99b1e8b36a50cdb09ba0
huynhp724 <phuonghuynh1550@gmail.com>
2018-10-13 21:55:36 -0500

    Camera Shake

--- Chillenium2018/Assets/Scripts/Combat/PlayerChar.cs
+++ Chillenium2018/Assets/Scripts/Combat/PlayerChar.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using EZCameraShake;
 
 public class PlayerChar : MonoBehaviour {
 	public int score = 0;
@@ -15,12 +16,19 @@ public class PlayerChar : MonoBehaviour {
 	public int damageFromWeakness = 3;
 	public int damageFromStrength = 1;
 
+    public AudioClip weakHit;
+    public AudioClip normalHit;
+    public AudioClip strongHit;
+
+    public AudioSource playerAud;
+
     //public float blasterSpeed = 50f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		health = maxHealth;
 		spawn = new Vector2 (transform.position.x, transform.position.y);
+        playerAud = gameObject.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -37,7 +45,10 @@ public class PlayerChar : MonoBehaviour {
 		if (otherType == myType) { //if same type
 			Debug.Log(myType + " hit by its own type");
 			health -= damageFromSame;
-		}
+            playerAud.clip = normalHit;
+            playerAud.Play();
+            CameraShaker.Instance.ShakeOnce(2f, 4f, .1f, 1.5f);
+        }
 
 		//RPS:
 		//BASS beats GUITAR beats HORN beats...
@@ -45,28 +56,46 @@ public class PlayerChar : MonoBehaviour {
 			if (otherType == Entity.Element.bass) {	
 				Debug.Log ("guitar weak to bass");
 				health -= damageFromWeakness;
-			} else if (otherType == Entity.Element.horn) {
+                playerAud.clip = strongHit;
+                playerAud.Play();
+                CameraShaker.Instance.ShakeOnce(3f, 7f, .1f, 2f);
+            } else if (otherType == Entity.Element.horn) {
 				Debug.Log ("guitar strong against horn");
 				health -= damageFromStrength;
-			}
+                playerAud.clip = weakHit;
+                playerAud.Play();
+                CameraShaker.Instance.ShakeOnce(1f, 1f, .1f, 1f);
+            }
 		}
 		else if (myType == Entity.Element.bass) { //if col is bass
 			if (otherType == Entity.Element.horn) {	
 				Debug.Log ("bass weak to horn");
 				health -= damageFromWeakness;
-			} else if (otherType == Entity.Element.guitar) {
+                playerAud.clip = strongHit;
+                playerAud.Play();
+                CameraShaker.Instance.ShakeOnce(3f, 7f, .1f, 2f);
+            } else if (otherType == Entity.Element.guitar) {
 				Debug.Log ("bass strong against guitar");
 				health -= damageFromStrength;
-			}
+                playerAud.clip = weakHit;
+                playerAud.Play();
+                CameraShaker.Instance.ShakeOnce(1f, 1f, .1f, 1f);
+            }
 		}
 		else if (myType == Entity.Element.horn) { //if col is horn
 			if (otherType == Entity.Element.guitar){	
 				Debug.Log ("horn weak to guitar");
 				health -= damageFromWeakness;
-			} else if (otherType == Entity.Element.bass) {
+                playerAud.clip = strongHit;
+                playerAud.Play();
+                CameraShaker.Instance.ShakeOnce(3f, 7f, .1f, 2f);
+            } else if (otherType == Entity.Element.bass) {
 				Debug.Log ("horn strong against bass");
 				health -= damageFromStrength;
-			}
+                playerAud.clip = weakHit;
+                playerAud.Play();
+                CameraShaker.Instance.ShakeOnce(1f, 1f, .1f, 1f);
+            }
 		}
         if (health <= 0)
         {
